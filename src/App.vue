6a78de587a938be74d1df3e86158fe754ca492f8
roronoa97 <harizulaiman@gmail.com>
2020-09-21 12:07:18 +0800

    customize movie api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="header">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  computed:{
+    getMovies(){
+      return this.$store.state.movies
+    }
+  },
+  mounted(){
+    this.$store.dispatch('getTrending');
+  }
+}
+</script>
+
 <style>
+*, *::before, *::after{
+  box-sizing: border-box;
+}
+body{
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(90deg, #000000, #422121);
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100vw;
+  height: 100vh;
+  color: #FF6363 
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+      movies: null
   },
   mutations: {
+      setMovies(state, movies){
+          state.movies = movies
+      }
   },
   actions: {
+      getTrending({commit}){
+        const api_key = process.env.VUE_APP_API_KEY
+        let media_type = "movie"
+        let time_window = "week"
+
+        
+        axios.get(`${process.env.VUE_APP_BASE_URL}/${media_type}/${time_window}?api_key=${api_key}`)
+            .then(res => {
+                let movies = res.data.results.slice(0, 10);
+                console.log(movies)
+                commit('setMovies', movies)
+            })
+      }
   },
   modules: {
   }
