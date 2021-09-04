fb88635bac02d3247958c9cb35be59a486497b3f
roronoa97 <harizulaiman@gmail.com>
2021-09-04 14:32:03 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+  mounted(){
+    this.$store.dispatch('getTrending');
+  }
+}
+</script>
+
+<style>
+*, *::before, *::after{
+  box-sizing: border-box;
+}
+body{
+  margin: 0;
+  padding: 0;
+  background: linear-gradient(90deg, #000000, #422121);
+  font-family: 'Roboto', 'Poppins',  sans-serif;
+  font-size: 1rem;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+  color:#ffbdbd
+}
+
+ul{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,71 @@
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+export default createStore({
+  state: {
+      movies: null,
+      currentMovie: null
+  },
+  mutations: {
+      setMovies(state, data){
+          let movies = data.movies
+          let allGenres = data.allGenres.genres
+          movies.forEach((movie, index) => {
+            let genresId = movie.genre_ids;
+            let moviesGenre = allGenres.filter( genre => genresId.includes(genre.id) );
+            movie.moviesGenre = moviesGenre
+            if(index == 0){
+                movie.selected = true;
+                state.currentMovie = index
+            }else{
+                movie.selected = false;
+            }
+          })
+          state.movies = movies
+      },
+      selectedMovie(state, movieId){
+            let movies = state.movies
+            movies.forEach((movie, index) => {
+                if(movie.id == movieId){
+                    movie.selected = true
+                    state.currentMovie = index
+                }else{
+                    movie.selected = false
+                }
+            })
+            state.movies = movies
+      }
+  },
+  getters: {
+      getMovies(state){
+          return state.movies
+      },
+      getCurrentMovie(state){
+          return state.currentMovie
+      }
+  },
+  actions: {
+      getTrending({dispatch}){
+        let media_type = "movie"
+        let time_window = "week"
+
+        axios.get(`${process.env.VUE_APP_BASE_URL}/trending/${media_type}/${time_window}?api_key=${process.env.VUE_APP_API_KEY}`)
+            .then(res => {
+                let movies = res.data.results.slice(0, 10);
+                dispatch('getGenres', movies)
+            })
+      },
+      selectMovie({commit}, movieId){
+          commit('selectedMovie', movieId)
+      },
+      getGenres({commit}, movies){
+          axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.VUE_APP_API_KEY}`)
+            .then(res => {
+                let allGenres = res.data
+                commit('setMovies', {movies, allGenres})
+            })
+      }
+  },
+  modules: {
+  }
+})
